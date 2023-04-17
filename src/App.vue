0497561277288cf0c3be5e31be3075e1bc9d0414
brian_ogola <brianochiengogola78@gmail.com>
2023-04-17 23:42:19 +0300

    Project2023

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script>
+import { RouterLink, RouterView } from 'vue-router'
+import HeaderComponent from './components/HeaderFooter/HeaderComponent.vue'
+import FooterComponent from './components/HeaderFooter/FooterComponent.vue'
+
+
+export default {
+  components: { HeaderComponent, FooterComponent }
+}
+
+</script>
+
+<template>
+  <header-component></header-component>
+
+  <RouterView />
+  
+  <footer-component></footer-component>
+ 
+</template>
+
+<style>
+</style>
