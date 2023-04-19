b6b66c4268b80511ff86277a2fb154b2dccb2eff
brian_ogola <brianochiengogola78@gmail.com>
2023-04-19 22:28:21 +0300

    project2023

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
 import App from './App.vue'
+
 import router from './router'
 
 
@@ -11,5 +12,7 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+
+
 
 app.mount('#app')
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
-import AdminHomeView from '../views/AdminVeiw/AdminHomeView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -9,11 +8,6 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: HomeView
-    },
-    {
-      path: '/admin',
-      name: 'adminhome',
-      component: AdminHomeView
     },
     {
       path: '/about',
