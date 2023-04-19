32877c2a1c27da27d878600d02ec26b59e945394
brian_ogola <brianochiengogola78@gmail.com>
2023-04-19 13:40:13 +0300

    project2023

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,14 @@
 import { RouterLink, RouterView } from 'vue-router'
 import HeaderComponent from './components/HeaderFooter/HeaderComponent.vue'
 import FooterComponent from './components/HeaderFooter/FooterComponent.vue'
+import AdminHeaderComponent from './components/Admin/AdminAdminHeaderComponent.vue'
+import AdminFooterComponent from './components/Admin/AdminFooterComponent.vue'
+
 
 
 export default {
-  components: { HeaderComponent, FooterComponent }
+  components: { HeaderComponent, FooterComponent, AdminHeaderComponent,AdminFooterComponent },
 }
-
 </script>
 
 <template>
